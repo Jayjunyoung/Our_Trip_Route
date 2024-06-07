56c8fffb62b752f72c311be9c3b449eb9e3b43f2
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-06-07 16:55:36 +0900

    feat: 사이드바에 시작날짜, 종료날짜 비교에 따른 랜더링 구현

--- src/components/TripPlanSideBar.tsx
+++ src/components/TripPlanSideBar.tsx
@@ -8,7 +8,7 @@ const TripPlanSideBar: React.FC = () => {
     <div className="top-0 left-0 flex flex-col justify-start items-center w-[300px] h-full bg-blue-50 z-50 max-w-[300px] px-4 py-10">
       <h2 className="text-2xl font-bold mb-4">선택된 날짜</h2>
       <ul className="space-y-2">
-        {startDate && endDate ? (
+        {startDate && endDate && startDate <= endDate ? (
           <li className="mb-2 text-lg">
             {startDate} ~ {endDate}
           </li>
