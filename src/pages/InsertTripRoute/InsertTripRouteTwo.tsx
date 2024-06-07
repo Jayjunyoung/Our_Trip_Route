29dd59d412d0de3f79d64bf06a6e074d11b691de
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-06-07 16:56:08 +0900

    design: overflow-scroll 적용 부분 수정

--- src/pages/InsertTripRoute/InsertTripRouteTwo.tsx
+++ src/pages/InsertTripRoute/InsertTripRouteTwo.tsx
@@ -6,9 +6,9 @@ export default function InsertTripRouteTwo() {
   const { selectedDates } = useDateStore();
 
   return (
-    <div className="flex justify-between w-full h-full overflow-scroll">
+    <div className="flex justify-between w-full h-full">
       <TripPlanSideBar />
-      <div className="flex-grow px-8 py-10 space-y-10 box-border w-full">
+      <div className="flex-grow px-8 py-10 space-y-10 box-border w-full h-full overflow-scroll">
         <div className="flex flex-col justify-evenly items-center w-full h-full">
           {selectedDates.map((date, index) => (
             <TripRouteCard key={index} day={`day${index + 1}`} />
